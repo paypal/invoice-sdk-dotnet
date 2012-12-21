87a8f9cc9b38705b1550843ab356ce3a5df6e211
unknown <rsomaskandan@D-MAA-00391891.corp.ebay.com>
2012-12-21 21:23:45 +0530

    Updated folder structure for NuGet

--- Invoice.bat
+++ Invoice.bat
@@ -1,3 +1,3 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 8\Common7\IDE\devenv.com" PayPal_Invoicing_SDK.sln /build Release %1
+call "C:\Program Files (x86)\Microsoft Visual Studio 8\Common7\IDE\devenv.com" PayPal_Invoicing_SDK\PayPal_Invoicing_SDK.sln /build Release %1
 
 copy /Y PayPal_Invoicing_SDK\bin\Release\PayPal_Invoicing_SDK.dll Samples\InvoiceSampleApp\lib\. 
