285e4dbb278eb8e92696c91ebdf658c94a443d4d
unknown <rsomaskandan@D-MAA-00391891.corp.ebay.com>
2012-12-24 14:57:25 +0530

    Renamed Invoicing folder and file (except dll) names as Invoice for NuGet

--- Invoice.bat
+++ Invoice.bat
@@ -1,3 +1,3 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 8\Common7\IDE\devenv.com" PayPal_Invoicing_SDK\PayPal_Invoicing_SDK.sln /build Release %1
+call "C:\Program Files (x86)\Microsoft Visual Studio 8\Common7\IDE\devenv.com" PayPal_Invoice_SDK\PayPal_Invoice_SDK.sln /build Release %1
 
-copy /Y PayPal_Invoicing_SDK\bin\Release\PayPal_Invoicing_SDK.dll Samples\InvoiceSampleApp\lib\. 
+copy /Y PayPal_Invoice_SDK\bin\Release\PayPal_Invoicing_SDK.dll Samples\InvoiceSampleApp\lib\. 
